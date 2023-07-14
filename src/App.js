e7eb0253b9a8a15fe3ba6111e4173b9540cec0e8
xkostolanskym <kostolansky.marek@gmail.com>
2023-07-14 21:33:28 +0200

    data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Break from "./components/Break";
+import Product from "./components/Product";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <Break />
+      <Product />
     </div>
   );
 }
---
+++ src/components/Break.jsx
@@ -0,0 +1,11 @@
+import leaves from "../images/leaves.png";
+
+const Break = () => {
+  return (
+    <div className="container d-flex justify-content-center">
+      <img className="img-fluid mt-3" src={leaves} />
+    </div>
+  );
+};
+
+export default Break;
